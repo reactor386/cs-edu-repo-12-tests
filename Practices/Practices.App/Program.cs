1c3584fa749665062e43572c4f58af332dd49150
reactor386 <reactor386@gmail.com>
2025-01-14 00:30:47 +0300

    Реализация необходимого функционала
     - добавлены тесты для класса Calculator
     - метод Main дополнен исполняемым кодом для наглядности
     - обновлен файл CHANGELOG.md

--- Practices/Practices.App/Program.cs
+++ Practices/Practices.App/Program.cs
@@ -8,6 +8,19 @@ internal class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World!");
+        Console.WriteLine("Calculator");
+        Console.WriteLine("---");
+        int result = 0;
+
+        var calculator = new Calculator();
+
+        Console.WriteLine($"Additional: 1+2={calculator.Additional(1, 2)}");
+        Console.WriteLine($"Subtraction: 3-2={calculator.Subtraction(3, 2)}");
+        Console.WriteLine($"Miltiplication: 2*3={calculator.Miltiplication(2, 3)}");
+        Console.WriteLine($"Division: 4/2={calculator.Division(4, 2)}");
+
+        Console.WriteLine("---");
+        Console.WriteLine($"return: [{result}]");
+        Console.ReadKey();
     }
 }
